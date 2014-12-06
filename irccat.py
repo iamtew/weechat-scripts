f8f27dc99943f78ea3e78d195bba3abdd7458c65
Mikael Sörlin <iamtew@asylunatic.se>
2014-12-06 22:44:00 +0100

    message acceptiation proof of concept
    Not good enough, currently it blocks until a message is received, and
    then it stops listening. Baby steps...

--- irccat.py
+++ irccat.py
@@ -13,6 +13,9 @@ SCRIPT_VERSION = "0.1.dev1"
 SCRIPT_LICENSE = "MIT"
 SCRIPT_DESC    = "Network socket listener"
 
+IRCCAT_ADDR = 'localhost'
+IRCCAT_PORT = 5234
+
 try:
     import weechat
     from weechat import prnt
@@ -24,8 +27,51 @@ except ImportError:
     import_ok = False
 
 
+import socket
+
+
+def irccat_socket_open(addr, port):
+    """
+    Open our listener socket.
+
+    :return: socket pointer
+    """
+    prnt(irccat_buffer, 'Opening socket on ' + str(addr) + ':' + str(port))
+    rsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rsocket.bind((addr, int(port)))
+    rsocket.listen(5)
+
+    return rsocket
+
+
+def irccat_readmsg():
+    while True:
+        conn, addr = irccat_socket.accept()
+        data = conn.recv(1024)
+        prnt(irccat_buffer, 'Message: ' + data)
+        conn.close()
+
+
+def irccat_socket_close(socket):
+    """
+    Close our listening socket
+    """
+    prnt(irccat_buffer, 'Closing socket')
+
+
+def irccat_load_config():
+    """
+    Load our configuration
+    """
+    prnt(irccat_buffer, 'Loading configuration')
+
+
 def irccat_start():
-    """Startup function"""
+    """
+    Startup function
+
+    :return: irccat buffer pointer
+    """
     buffer = weechat.buffer_new('irccat', '', '', '', '')
     weechat.buffer_set(buffer, 'title', '[irccat]')
 
@@ -33,9 +79,15 @@ def irccat_start():
 
 
 def irccat_end():
-    """Cleanup function"""
+    """
+    Cleanup function
+
+    :return: weechat OK
+    """
+    irccat_socket_close(irccat_socket)
     weechat.buffer_close(irccat_buffer)
-    return weechat.WEECHAT_RC_OK
+
+    return WEECHAT_RC_OK
 
 
 if __name__ == '__main__' and import_ok and \
@@ -48,5 +100,9 @@ if __name__ == '__main__' and import_ok and \
                          'UTF-8'):
 
     irccat_buffer = irccat_start()
+    irccat_socket = irccat_socket_open(IRCCAT_ADDR, IRCCAT_PORT)
 
-
+    conn, addr = irccat_socket.accept()
+    data = conn.recv(1024)
+    prnt(irccat_buffer, 'Message: ' + data)
+    conn.close()
